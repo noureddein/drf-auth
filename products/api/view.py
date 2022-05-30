ee208ca04afd8e0d280d84d61e7949d9b13af89f
noureddein <noureddein@gmail.com>
2022-05-31 00:36:17 +0300

    Add gunicorn, whitenois, update docker-compose, and change the authentication of detail view

--- products/api/view.py
+++ products/api/view.py
@@ -1,19 +1,20 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from products.models import Product
 from .serializer import ProductSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
 
 class ProductsListView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 
 class ProductDetailView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
